431d8a97cb9c9635c85bd63f6207098e8ed317f7
mohamedabdelrahman13 <mohamedrmcf897@gmail.com>
2025-04-06 19:45:13 +0200

    gap between services of home page done

--- palmClinic/src/app/Environment/environment.prod.ts
+++ palmClinic/src/app/Environment/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://smarterapalm-001-site1.qtempurl.com/api',
+    // apiBaseUrl: 'https://smarterapalm-001-site1.qtempurl.com/api',
+    apiBaseUrl: 'http://palmclinic.runasp.net/api',
     debugMode: false
   };
